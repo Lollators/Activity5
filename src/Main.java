0e4a8b94c928eb839c296163f6e7762167c8d538
mbmarks2816 <mbmarks2816@eagle.fgcu.edu>
2018-09-26 11:22:56 -0400

    you know, I finished it in

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.sql.SQLOutput;
+
 public class Main {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        System.out.println("Hello World!");
-    }
+    //  Write a static main method that creates two objects,
+    // one with the default constructor and the other with the
+    //  constructor with parameters.  Give sample
+    //  data for the parameters
+    VehicleChassis myVc = new VehicleChassis();
+    myVc.setChassisType("chazzz");
+    System.out.println(myVc.toString());
+    System.out.println(myVc.getChassisType());
+    VehicleChassis myVc2 = new VehicleChassis("chas");
+    System.out.println(myVc2.toString());
+  }
 }
